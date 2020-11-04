ecaca569ed51400f5a7f6dd51aff6f8012b7f7f4
Sriprad Potukuchi <sripradpotukuchi@gmail.com>
2020-11-04 13:42:51 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Demo of the XOR logic gate:
+
+from NeuralNetwork import *
+
+nn = NeuralNetwork([2, 2, 1])
+
+inputs = [[1, 0], [0, 1], [1, 1], [0, 0]]
+targets = [[1], [1], [0], [0]]
+
+nn.train(inputs, targets)
+
+print('---------------')
+
+for i in inputs:
+	print(f'input: {i}; output: {nn.feedforward(i)}')
